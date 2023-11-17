9f45a902569d9c298db4d7b0287a66d019f1858a
Allan-Wachira <aldocky@gmail.com>
2023-11-17 03:07:57 -0800

    commit 1

--- 1-digit.c
+++ 1-digit.c
@@ -1,26 +1,49 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <limits.h>
 
-/**
- * init_params - clears struct fields and reset buf
- * @params: the parameters struct
- * @ap: the argument pointer
- * Return: void
- */
+// Custom struct for handling parameters
+typedef struct {
+    int unsign;
+    int plus_flag;
+    int space_flag;
+    int hashtag_flag;
+    int zero_flag;
+    int minus_flag;
+    int width;
+    unsigned int precision;
+    int h_modifier;
+    int l_modifier;
+} custom_params_t;
 
-void init_params(params_t *params, va_list ap)
-{
-	params->unsign = 0;
+// Function to initialize custom parameters
+void init_custom_params(custom_params_t *custom_params, va_list ap) {
+    custom_params->unsign = 1; // Modified to set unsign to 1 initially
 
-	params->plus_flag = 0;
-	params->space_flag = 0;
-	params->hashtag_flag = 0;
-	params->zero_flag = 0;
-	params->minus_flag = 0;
+    // Additional flags or options can be added here
+    custom_params->custom_flag = 0; // Example of a custom flag
 
-	params->width = 0;
-	params->precision = UINT_MAX;
+    custom_params->width = 0;
+    custom_params->precision = UINT_MAX;
+    custom_params->h_modifier = 0;
+    custom_params->l_modifier = 0;
+    (void)ap;
+}
 
-	params->h_modifier = 0;
-	params->l_modifier = 0;
-	(void)ap;
+int main() {
+    // Example usage
+    custom_params_t params;
+    va_list ap;
+    
+    // Initialize parameters
+    va_start(ap, params);
+    init_custom_params(&params, ap);
+    va_end(ap);
+    
+    // Use the initialized parameters for further processing or formatting
+    // This can involve a custom output function or any other task requiring formatted parameters
+    // For example:
+    printf("Custom parameters initialized. Width: %d, Precision: %u\n", params.width, params.precision);
+    
+    return 0;
 }
